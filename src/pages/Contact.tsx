b78210d60a7d4b6fc6371d6131be6a3433a8a8de
zak2025533 <zakarianabil1994@gmail.com>
2025-06-23 16:42:16 +0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import SimpleFooter from "@/components/SimpleFooter";
+//import SimpleFooter from "@/components/SimpleFooter";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
