2c46624b7016c296a09f87845ff1b0a486b74ec1
zak2025533 <zakarianabil1994@gmail.com>
2025-06-23 17:48:28 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import tailwindDir from "tailwindcss-dir"; // دعم الاتجاهات
+//import tailwindDir from "tailwindcss-dir"; // دعم الاتجاهات
 
 export default {
   darkMode: ["class"],
