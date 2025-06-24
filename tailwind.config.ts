f72131726214f98422719e72cd91121490468a73
zak2025533 <zakarianabil1994@gmail.com>
2025-06-24 20:54:43 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,23 +11,27 @@ export default {
   prefix: "",
   theme: {
     container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '1rem',
-        md: '1.5rem',
-        lg: '2rem',
-        xl: '2.5rem',
-        '2xl': '3rem',
-      },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1400px',
-      },
-    },
+  center: true,
+  padding: {
+    DEFAULT: '1rem',
+    sm: '1rem',
+    md: '1.5rem',
+    lg: '2rem',
+    xl: '2.5rem',
+    '2xl': '3rem',
+  },
+  screens: {
+    xs: '375px',
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1400px',
+    '3xl': '1600px',
+    '4xl': '1920px',
+  },
+},
+
     extend: {
       colors: {
         border: 'hsl(var(--border))',
